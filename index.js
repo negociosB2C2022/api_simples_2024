667e75682f262af62ab96f628a276e02437ab3f3
negociosB2C2022 <negocios.b2c.2022@gmail.com>
2024-06-11 08:31:51 -0300

    v_2

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express= require("express")
 const app= express()
 const cors= require('cors')
 require('dotenv').config()
+const mogoose= require("mongoose")
 
 //configurando a leitura de JSON (midwares)
 app.use(
@@ -19,8 +20,39 @@ app.get('/', (req, res)=>{
     res.json({message: 'Tempo para atulização da api'})
 })
 
+//pegando todas as coordenadas
+const Coordenadas= require("./Coordenadas")
+app.get("/coordenadas", async(req, res)=>{
+    try {
+        const coord= await Coordenadas.find()
+        res.status(200).json({
+            mensagem:"Todas as coordenadas logo abaixo.",
+            coord: coord
+        })
+
+    } catch (error) {
+        res.status(200).json({mensagem: "Ocorreu um erro!"})
+    }
+})
+
 
 
 //criar uma parta acessar a API
+/*
 const porta= process.env.PORT || 3000
 app.listen(porta)
+*/
+//conectando com o banco de dados 
+const BD_USER= process.env.BD_USER
+const BD_PASSWORD= encodeURIComponent(process.env.BD_PASSWORD)
+const NAME_BD= process.env.NAME_BD
+mogoose.connect(
+    `mongodb+srv://${BD_USER}:${BD_PASSWORD}@cluster0.hhgpc0q.mongodb.net/${NAME_BD}?retryWrites=true&w=majority&appName=Cluster0`,
+)
+.then(()=>{
+    const port = process.env.PORT|| 4000
+    //criar uma parta acessar a API
+    app.listen(port)
+    console.log("Conexão realizada. Porta: "+ port)
+})
+.catch((error)=> console.log(error))
