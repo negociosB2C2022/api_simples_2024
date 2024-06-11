f1fc22fa67f52d5e73043c7137618a0c46ed0ab7
negociosB2C2022 <negocios.b2c.2022@gmail.com>
2024-06-11 08:12:56 -0300

    v_1

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express= require("express")
 const app= express()
 const cors= require('cors')
+require('dotenv').config()
 
 //configurando a leitura de JSON (midwares)
 app.use(
@@ -15,10 +16,11 @@ app.use(cors())
 
 //criar rota inicial 
 app.get('/', (req, res)=>{
-    res.json({message: 'única rota disponível'})
+    res.json({message: 'Tempo para atulização da api'})
 })
 
 
 
 //criar uma parta acessar a API
-app.listen(3000)
+const porta= process.env.PORT || 3000
+app.listen(porta)
